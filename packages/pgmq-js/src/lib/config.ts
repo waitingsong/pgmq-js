3926e13c1f2cb0d8ef6f7492fc173bda467454d6
waiting <1661926154@qq.com>
2025-01-12 23:18:29 +0800

    feat(pgmq): initialize environment with Pgmq.initEnvironment()

--- packages/pgmq-js/src/lib/config.ts
+++ packages/pgmq-js/src/lib/config.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import type { DbConfig, DbConnectionConfig } from './types.js'
 
 
@@ -16,6 +19,18 @@ export const initDbConfigPart: Omit<DbConfig, 'connection'> = {
     min: 0,
     max: 100,
     // propagateCreateError: false,
+    afterCreate: (conn: any, done: any) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      conn.query('SET search_path TO pgmq, public;', (err: unknown) => {
+        if (err) {
+          // first query failed,
+          // return error and don't try to make next query
+          done(err, conn)
+          return
+        }
+        done(null, conn)
+      })
+    },
   },
   acquireConnectionTimeout: 30000,
 }
