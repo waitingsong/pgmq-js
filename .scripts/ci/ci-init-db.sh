2da97439f283a623be09669341a82cda72fd2ca6
waiting <1661926154@qq.com>
2025-01-13 16:02:05 +0800

    ci: 6

--- .scripts/ci/ci-init-db.sh
+++ .scripts/ci/ci-init-db.sh
@@ -13,12 +13,8 @@ psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -c "CREATE USER dbuser
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -c "\du+;"
 # psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -c "SELECT usename, usecreatedb, usesuper, userepl, usebypassrls, valuntil, useconfig FROM pg_catalog.pg_user;"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SELECT e.extname AS extension_name, \
-  n.nspname AS schema_name, \
-  r.rolname AS owner \
-  FROM pg_extension e \
-  JOIN pg_authid r ON e.extowner = r.oid \
-  JOIN pg_catalog.pg_namespace n ON n.oid = e.extnamespace \
-  WHERE e.extname = 'pgmq' OR e.extname = 'pg_partman'; "
+  n.nspname AS schema_name, r.rolname AS owner FROM pg_extension e \
+  JOIN pg_authid r ON e.extowner = r.oid JOIN pg_catalog.pg_namespace n ON n.oid = e.extnamespace; "
 echo 11
 echo -e "\n"
 
