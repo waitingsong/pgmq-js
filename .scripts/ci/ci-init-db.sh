73d7cf8d92049f43da0c397416b62d8beeef808b
waiting <1661926154@qq.com>
2025-01-13 15:13:32 +0800

    ci: 4

--- .scripts/ci/ci-init-db.sh
+++ .scripts/ci/ci-init-db.sh
@@ -8,7 +8,7 @@ dig postgres
 echo $DBUSER
 echo $DBUSER_PWD
 
-PGPASSWORD="$PGPASSWORD"
+export PGPASSWORD="$PGPASSWORD"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SHOW TIMEZONE;"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -c "CREATE USER dbuser WITH PASSWORD 'dbuser';"
 echo -e "\n"
@@ -18,7 +18,7 @@ SQL_DIR="$cwd/packages/pgmq-js/database/"
 cd "$SQL_DIR"
 . ./init-db.sh
 
-PGPASSWORD="$PGPASSWORD"
+export PGPASSWORD="$PGPASSWORD"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SELECT extname, extversion FROM pg_extension;"
 echo "\l" | psql -h $POSTGRES__HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d postgres
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "\d+"
