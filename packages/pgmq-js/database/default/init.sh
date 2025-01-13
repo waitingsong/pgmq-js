25aa14f30351b6b120c2ff93dc1ee42409bbf4ca
waiting <1661926154@qq.com>
2025-01-13 16:37:50 +0800

    ci: 17

--- packages/pgmq-js/database/default/init.sh
+++ packages/pgmq-js/database/default/init.sh
@@ -5,21 +5,22 @@ echo -e "\n"
 
 export PGPASSWORD="$DBUSER_PWD"
 echo PGPASSWORD: $PGPASSWORD
-psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DB_USER -d $POSTGRES_DB \
+echo DB: $POSTGRES_DB
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $DB_USER -d $POSTGRES_DB \
   -f ddl/extension.sql \
 
 echo 10
-psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DB_USER -d $POSTGRES_DB \
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $DB_USER -d $POSTGRES_DB \
   -f ddl/tb_queue_meta.sql \
 
 echo 11
-psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DB_USER -d $POSTGRES_DB \
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $DB_USER -d $POSTGRES_DB \
   -f ddl/tb_route.sql \
 
 
 echo 333
 export PGPASSWORD="$POSTGRES_PWD"
 echo PGPASSWORD: $PGPASSWORD
-psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB \
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB \
   -f ddl/init-privilege.sql \
 
