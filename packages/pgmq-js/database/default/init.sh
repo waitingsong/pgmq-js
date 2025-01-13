b24c825abfdc0895b3977945cdb731c0e8011fcd
waiting <1661926154@qq.com>
2025-01-13 16:08:57 +0800

    ci: 8

--- packages/pgmq-js/database/default/init.sh
+++ packages/pgmq-js/database/default/init.sh
@@ -3,13 +3,19 @@ set -e
 
 echo -e "\n"
 
-export PGPASSWORD="$DBUSER_PWD"
 echo 222
 echo $DBUSER
 echo $DBUSER_PWD
 echo $PGPASSWORD
+export PGPASSWORD="$POSTGRES_PWD"
+echo $PGPASSWORD
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB \
+  -f ddl/ci-config.sql \
+
+echo 333
+export PGPASSWORD="$DBUSER_PWD"
+echo $PGPASSWORD
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DB_USER -d $POSTGRES_DB \
-  # -f ddl/ci-config.sql \
   -f ddl/extension.sql \
   -f ddl/tb_queue_meta.sql \
   -f ddl/tb_route.sql \
