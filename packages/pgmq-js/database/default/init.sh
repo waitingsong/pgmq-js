dd7bb5c3d147db948a78c80da310d080f75d2b09
waiting <1661926154@qq.com>
2025-01-13 17:26:53 +0800

    ci: 3

--- packages/pgmq-js/database/default/init.sh
+++ packages/pgmq-js/database/default/init.sh
@@ -5,20 +5,16 @@ echo -e "\n"
 
 export PGPASSWORD="$DBUSER_PWD"
 # Execute with the normal user
-echo 100
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DBUSER -d $POSTGRES_DB \
   -f ddl/extension_user.sql \
-
-echo 102
-psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DBUSER -d $POSTGRES_DB \
   -f ddl/tb_queue_meta.sql \
   -f ddl/tb_route.sql \
 
 
-echo 103
 export PGPASSWORD="$POSTGRES_PWD"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB \
   -f ddl/extension.sql \
 
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "\d+"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "\dt+ pgmq.*"
+
