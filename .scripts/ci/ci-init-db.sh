cbba078a3e36a8b8aa7004634a05e3f085a3b329
waiting <1661926154@qq.com>
2025-01-13 18:01:32 +0800

    ci: 6

--- .scripts/ci/ci-init-db.sh
+++ .scripts/ci/ci-init-db.sh
@@ -14,6 +14,7 @@ SQL_DIR="$cwd/packages/pgmq-js/database/"
 cd "$SQL_DIR"
 . ./init-db.sh
 
+echo -e "\nInit post"
 export PGPASSWORD="$POSTGRES_PWD"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -f $cwd/.scripts/ci/init-post.sql
 
