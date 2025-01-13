f6784554105726afe4db78d144d6dffc253f36d3
waiting <1661926154@qq.com>
2025-01-13 17:20:16 +0800

    ci: 9

--- .scripts/ci/init-post.sql
+++ .scripts/ci/init-post.sql
@@ -1,8 +1,7 @@
 
-\l;
 \d+;
 \du+;
-SELECT e.extname AS extension_name, \
-  n.nspname AS schema_name, r.rolname AS owner FROM pg_extension e \
+SELECT e.extname AS extension_name,
+  n.nspname AS schema_name, r.rolname AS owner FROM pg_extension e
   JOIN pg_authid r ON e.extowner = r.oid JOIN pg_catalog.pg_namespace n ON n.oid = e.extnamespace;
 
