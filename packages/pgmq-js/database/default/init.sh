7628590ecb68c6667db24f478646111fac77b6b1
waiting <1661926154@qq.com>
2025-01-13 16:36:07 +0800

    ci: 15

--- packages/pgmq-js/database/default/init.sh
+++ packages/pgmq-js/database/default/init.sh
@@ -3,21 +3,23 @@ set -e
 
 echo -e "\n"
 
-echo 222
-echo $DBUSER
-echo $DBUSER_PWD
-echo $PGPASSWORD
 export PGPASSWORD="$DBUSER_PWD"
-echo $PGPASSWORD
+echo PGPASSWORD: $PGPASSWORD
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DB_USER -d $POSTGRES_DB \
   -f ddl/extension.sql \
+
+echo 10
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DB_USER -d $POSTGRES_DB \
   -f ddl/tb_queue_meta.sql \
+
+echo 11
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DB_USER -d $POSTGRES_DB \
   -f ddl/tb_route.sql \
 
 
 echo 333
 export PGPASSWORD="$POSTGRES_PWD"
-echo $PGPASSWORD
+echo PGPASSWORD: $PGPASSWORD
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB \
   -f ddl/init-privilege.sql \
 
