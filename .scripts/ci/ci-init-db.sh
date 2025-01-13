066e81cd8f925061a6077c70714533de9fcc5e14
waiting <1661926154@qq.com>
2025-01-13 14:24:19 +0800

    ci: init

--- .scripts/ci/ci-init-db.sh
+++ .scripts/ci/ci-init-db.sh
@@ -5,9 +5,9 @@ psql -V
 # netstat -tunpl
 dig postgres
 
-PGPASSWORD="$PGMQ_PASSWORD"
-psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "SHOW TIMEZONE;"
-
+PGPASSWORD="$PGPASSWORD"
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SHOW TIMEZONE;"
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "CREATE USER $DBUSER WITH PASSWORD '$DBPASSWORD';"
 echo -e "\n"
 
 
@@ -15,8 +15,9 @@ SQL_DIR="$cwd/packages/pgmq-js/database/"
 cd "$SQL_DIR"
 . ./init-db.sh
 
-psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "SELECT extname, extversion FROM pg_extension;"
-echo "\l" | psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d postgres
-psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -c "\d+"
+PGPASSWORD="$PGPASSWORD"
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "SELECT extname, extversion FROM pg_extension;"
+echo "\l" | psql -h $POSTGRES__HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d postgres
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -c "\d+"
 cd "$cwd"
 
--- packages/pgmq-js/database/default/init.sh
+++ packages/pgmq-js/database/default/init.sh
@@ -3,11 +3,11 @@ set -e
 
 echo -e "\n"
 
-PGPASSWORD="$PGMQ_PASSWORD"
+PGPASSWORD="$DBUSER_PWD"
 psql -h $PGMQ_HOST -p $PGMQ_PORT -U$PGMQ_USER -d $PGMQ_DB -bq \
   -f ddl/ci-config.sql \
-  # -f ddl/extension.sql \
-  # -f ddl/tb_queue_meta.sql \
-  # -f ddl/tb_route.sql \
+  -f ddl/extension.sql \
+  -f ddl/tb_queue_meta.sql \
+  -f ddl/tb_route.sql \
 
 
