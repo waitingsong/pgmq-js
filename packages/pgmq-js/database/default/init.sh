eaa193331738e4d711b61162896859d182fb7c62
waiting <1661926154@qq.com>
2025-01-15 23:19:58 +0800

    ci: 12

--- packages/pgmq-js/database/default/init.sh
+++ packages/pgmq-js/database/default/init.sh
@@ -6,6 +6,7 @@ echo -e "\n"
 export PGPASSWORD="$DBUSER_PWD"
 # Execute by the normal user
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DBUSER -d $POSTGRES_DB -bq -f ddl/extension_user.sql
+psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$DBUSER -d $POSTGRES_DB -c "SELECT schema_name FROM information_schema.schemata WHERE catalog_name = 'db_ci_test';"
 
 export PGPASSWORD="$POSTGRES_PWD"
 psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -bq  \
